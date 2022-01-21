6288dea6b1491bcbe5d7145e83d39a056c72c72e
Luca Trece <44877959+Lucatrevo@users.noreply.github.com>
2022-01-20 21:01:05 -0300

    Add files via upload

---
+++ challange2.java
@@ -0,0 +1,10 @@
+public class challange2 {
+	public static void main(String[] args) {
+		
+		System.out.print("Name: Luca Trece");
+		
+		System.out.print("\nNationality: Brazilian");
+		
+		System.out.print("\nBiggest Dream: Achieve financial independence and have my own family");
+	}
+}
